ce0a095a822d3bf00302d0c2734a71ada0c3adce
maehachi08 <maehachi08@gmail.com>
2016-08-10 01:17:59 +0900

    UstreamAPI のVideoデータ取得URLを変更

--- lib/goosetune/ustream.rb
+++ lib/goosetune/ustream.rb
@@ -1,6 +1,8 @@
 class Goosetune::Ustream
   def initialize
-    url = 'http://api.ustream.tv/json/user/6018269/listAllVideos/?limit=100'
+    # playyouhouse user_id: 6018269
+    # playyouhouse channel_id: 4509151
+    url = 'https://api.ustream.tv/channels/4509151/videos.json?limit=100'
     uri = URI.parse(URI.escape(url))
     json = Net::HTTP.get(uri)
     @response = JSON.parse(json)
